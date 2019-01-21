d0c980e380f6086c4ed8c491e0a34c55e32ec10c
jaime123 <jaihernandez@deloitte.com>
2019-01-21 13:57:38 -0500

    ng matrix code for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,22 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+
+  get <- function() x
+  setinv <- function(inv) i <<- inv
+  getinv <- function() i
+
+  list(set = set,
+       get = get,
+       setinverse = setinv,
+       getinverse = getinv)
+
 }
 
 
@@ -12,4 +28,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+  i <- x$getinv()
+  if (!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+
+  i
+
 }
